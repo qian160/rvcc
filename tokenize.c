6fd697c02916286afa32119a5ba80e8227cf21b2
nakashima <1601315694@qq.com>
2023-02-26 00:55:54 +0800

    [38] 支持十六进制转义字符

--- tokenize.c
+++ tokenize.c
@@ -62,17 +62,42 @@ Token *newToken(TokenKind Kind, char *Start, char *End) {
     return Tok;
 }
 
+// 返回一位十六进制转十进制
+// hexDigit = [0-9a-fA-F]
+// 16: 0 1 2 3 4 5 6 7 8 9  A  B  C  D  E  F
+// 10: 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+static int fromHex(char C) {
+    if ('0' <= C && C <= '9')
+        return C - '0';
+    if ('a' <= C && C <= 'f')
+        return C - 'a' + 10;
+    return C - 'A' + 10;
+}
+
 // 读取转义字符
 static int readEscapedChar(char **NewPos, char * P) {
+    // 八进制转义字符
     if ('0' <= *P && *P <= '7') {
         // 读取一个八进制数字，不能长于三位
         // \abc = (a*8+b)*8+c
-        int C = *P++ - '0';
-        if ('0' <= *P && *P <= '7') {
-            C = (C << 3) + (*P++ - '0');
-            if ('0' <= *P && *P <= '7')
-                C = (C << 3) + (*P++ - '0');
-        }
+        int C = 0, n = 0;
+        for(; '0' <= *P && *P <= '7' && n++ < 3; P++)
+            C = (C << 3) + *P - '0';
+        *NewPos = P;
+        return C;
+    }
+    // 十六进制转义字符
+    if (*P == 'x') {
+        P++;
+        // 判断是否为十六进制数字
+        if (!isxdigit(*P))
+            errorAt(P, "invalid hex escape sequence");
+
+        int C = 0;
+        // 读取一位或多位十六进制数字
+        // \xWXYZ = ((W*16+X)*16+Y)*16+Z
+        for (; isxdigit(*P); P++)
+            C = (C << 4) + fromHex(*P);
         *NewPos = P;
         return C;
     }
