f4ef74b1fad30fc927468184e48e580c24988cfb
nakashima <1601315694@qq.com>
2023-07-19 01:55:59 +0800

    [203] 支持va_copy()

--- include/stdarg.h
+++ include/stdarg.h
@@ -4,19 +4,21 @@
 typedef void *va_list;
 
 #define va_start(ap, last)                                                     \
-    do {                                                                         \
-        ap = __va_area__;                                                          \
-    } while (0)
+  do {                                                                         \
+    ap = __va_area__;                                                          \
+  } while (0)
 
 #define va_end(ap)
 
 // [196] 支持 va_arg()
 #define va_arg(ap, type)                                                       \
-    ({                                                                           \
-        type val = *(type *)ap;                                                    \
-        ap += 8;                                                                   \
-        val;                                                                       \
-    })
+  ({                                                                           \
+    type val = *(type *)ap;                                                    \
+    ap += 8;                                                                   \
+    val;                                                                       \
+  })
+
+#define va_copy(dest, src) (dest = src)
 
 #define __GNUC_VA_LIST 1
 typedef va_list __gnuc_va_list;
