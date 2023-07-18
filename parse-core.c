8978201ab53c8d749003c38363f795ed262ee871
nakashima <1601315694@qq.com>
2023-07-19 02:01:26 +0800

    [204] 支持解引用函数

--- parse-core.c
+++ parse-core.c
@@ -1725,7 +1725,12 @@ static Node *unary(Token **Rest, Token *Tok) {
         return newUnary(ND_NEG, cast(Rest, Tok->Next), Tok);
     // "*" cast. pointer
     if (equal(Tok, "*")) {
-        return newUnary(ND_DEREF, cast(Rest, Tok->Next), Tok);
+        Node *Nd = cast(Rest, Tok->Next);
+        addType(Nd);
+        // 如果func是函数，那么*func等价于func
+        if (Nd->Ty->Kind == TY_FUNC)
+            return Nd;
+        return newUnary(ND_DEREF, Nd, Tok);
     }
     // "*" cast. pointer
     if (equal(Tok, "&")) {
