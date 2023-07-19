581e1e2215096dcd74c708387208f45763a5b94c
nakashima <1601315694@qq.com>
2023-07-20 02:04:30 +0800

    [211] 禁止获取位域的地址

--- parse-core.c
+++ parse-core.c
@@ -1797,7 +1797,12 @@ static Node *unary(Token **Rest, Token *Tok) {
     }
     // "*" cast. pointer
     if (equal(Tok, "&")) {
-        return newUnary(ND_ADDR, cast(Rest, Tok->Next), Tok);
+        Node *LHS = cast(Rest, Tok->Next);
+        addType(LHS);
+        // 不能够获取位域的地址
+        if (LHS->Kind == ND_MEMBER && LHS->Mem->IsBitfield)
+            errorTok(Tok, "cannot take address of bitfield");
+        return newUnary(ND_ADDR, LHS, Tok);
     }
     if (equal(Tok, "!"))
         return newUnary(ND_NOT, cast(Rest, Tok->Next), Tok);
