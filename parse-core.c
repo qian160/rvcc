412c03b3e1b78b4443b0354877269193f107b25b
nakashima <1601315694@qq.com>
2023-07-27 03:04:14 +0800

    [253] [GNU] 支持?:使用操作数

--- parse-core.c
+++ parse-core.c
@@ -178,7 +178,7 @@
 // exprStmt = expr? ";"
 // expr = assign ("," expr)?
 // assign = conditional (assignOp assign)?
-// conditional = logOr ("?" expr ":" conditional)?
+// conditional = logOr ("?" expr? ":" conditional)?
 //      logOr = logAnd ("||" logAnd)*
 //      logAnd = bitOr ("&&" bitOr)*
 //      bitOr = bitXor ("|" bitXor)*
@@ -1594,7 +1594,7 @@ Node *assign(Token **Rest, Token *Tok) {
 }
 
 // 解析条件运算符
-// conditional = logOr ("?" expr ":" conditional)?
+// conditional = logOr ("?" expr? ":" conditional)?
 Node *conditional(Token **Rest, Token *Tok) {
     // logOr
     Node *Cond = logOr(&Tok, Tok);
@@ -1603,6 +1603,23 @@ Node *conditional(Token **Rest, Token *Tok) {
     if (!equal(Tok, "?")) {
         *Rest = Tok;
         return Cond;
+    }
+
+    // "?" ":"
+    if (equal(Tok->Next, ":")) {
+        // `A ?: B` 等价于 `Tmp = A, Tmp ? Tmp : B`
+        addType(Cond);
+        // Tmp
+        Obj *Var = newLVar("", Cond->Ty);
+        // Tmp = A
+        Node *LHS = newBinary(ND_ASSIGN, newVarNode(Var, Tok), Cond, Tok);
+        // Tmp ? Tmp : B
+        Node *RHS = newNode(ND_COND, Tok);
+        RHS->Cond = newVarNode(Var, Tok);
+        RHS->Then = newVarNode(Var, Tok);
+        RHS->Els = conditional(Rest, Tok->Next->Next);
+        // Tmp = A, Tmp ? Tmp : B
+        return newBinary(ND_COMMA, LHS, RHS, Tok);
     }
 
     // expr ":" conditional
