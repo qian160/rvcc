a086918dec267f381a2d6587c858cddf0d3535de
nakashima <1601315694@qq.com>
2023-08-06 02:05:41 +0800

    [304] 完善stdatomic.h

--- preprocess.c
+++ preprocess.c
@@ -809,7 +809,6 @@ static void initMacros(void) {
     defineMacro("__SIZEOF_SIZE_T__", "8");
     defineMacro("__SIZE_TYPE__", "unsigned long");
     defineMacro("__STDC_HOSTED__", "1");
-    defineMacro("__STDC_NO_ATOMICS__", "1");
     defineMacro("__STDC_NO_COMPLEX__", "1");
     defineMacro("__STDC_VERSION__", "201112L");
     defineMacro("__STDC__", "1");
